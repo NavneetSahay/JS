f6ddb42e9b21b272165758958ef94295618b34bd
NAVNEET SAHAY <56154963+NavneetSahay@users.noreply.github.com>
2020-04-24 22:31:56 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,10 @@
+const firstname = " NAVNEET "
+const lastname = " SAHAY "
+const val =  21
+const arr = [ 'string',21 , 
+function() { console.log('hi')},
+]
+arr [2] ()
+for(let i=0;i<arr.length; i++){
+    console.log(arr[i])
+}
---
+++ type.js
@@ -0,0 +1,6 @@
+const x = 21
+const y = 21.54
+
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof null)
